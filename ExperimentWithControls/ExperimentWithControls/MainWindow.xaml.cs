9ff5b0fec871be533181831d43b1985f3126c556
Collyn Christopher Brenner <collynbrenner@gmail.com>
2022-04-23 21:39:42 -0500

    end of chapter 2, on to chapter 3

--- ExperimentWithControls/ExperimentWithControls/MainWindow.xaml.cs
+++ ExperimentWithControls/ExperimentWithControls/MainWindow.xaml.cs
@@ -24,5 +24,63 @@ namespace ExperimentWithControls
         {
             InitializeComponent();
         }
+
+        private void number_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            // Character changes in one textbox renders to other textblock (which is not editable; only renders characters)
+            number.Text = numberTextBox.Text;
+        }
+
+        private void numberTextBox_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            // Allows only integers to be rendered, not text
+            // This runs before the input location (textbox) is updated and denies text chars from being entered/rendered
+            e.Handled = !int.TryParse(e.Text, out int result);
+        }
+
+        private void smallSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            number.Text = smallSlider.Value.ToString("0");
+        }
+
+        private void bigSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            number.Text = bigSlider.Value.ToString("000-000-0000");
+        }
+
+        private void RadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            if (sender is RadioButton radioButton)
+            {
+                number.Text = radioButton.Content.ToString();
+            }
+        }
+
+        private void myListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (myListBox.SelectedItem is ListBoxItem listBoxItem)
+            {
+                number.Text = listBoxItem.Content.ToString();
+            }
+        }
+
+        private void readOnlyComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (readOnlyComboBox.SelectedItem is ListBoxItem listBoxItem)
+            {
+                number.Text = listBoxItem.Content.ToString();
+            }
+        }
+
+        private void editableComboBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            // Event handler is completely different for editableComboBox than for readOnlyComboBox: 
+            // updates based on what is in the cmoboBox textbox nstead of on what is selected
+            // (because what is selected ends up in the textbox anyways - killing two birds with one stone)
+            if (sender is ComboBox comboBox)
+            {
+                number.Text = comboBox.Text;
+            }
+        }
     }
 }
