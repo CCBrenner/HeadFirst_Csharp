e19893a063608c9909b685b93faff33356460f7c
Collyn Christopher Brenner <collynbrenner@gmail.com>
2022-05-06 14:16:52 -0500

    intermittent work on ch4 project

---
+++ AbilityScoreTester/AbilityScoreTester/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+class AbilityScoreCalculator 
+{
+    public int RollResult = 14;
+    public double DivideBy = 1.75;
+    public int AddAmount = 2;
+    public int Minimum = 3;
+    public int Score;
+
+    public void CalculateAbilityScore()
+    {
+        // Divide the roll result by the Result field
+        double divided = RollResult / DivideBy;
+
+        // Add AddAmount ot the rsult of that division
+        int added = AddAmount += divided;
+
+        // If the result is too small , use Minimum
+        if (added < Minimum)
+        {
+            Score = Minimum;
+        }
+        else
+        {
+            Score = added;
+        }
+    }
+}
---
+++ typeCastingExpedition/typeCastingExpedition/Program.cs
@@ -0,0 +1,92 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+/*
+ *
+float myFloatValue = 10;
+int myIntvalue = (int) myFloatValue;
+Console.WriteLine("myIntValue is " + myIntvalue);
+*/
+
+/*
+int myInt = 10;
+
+byte myByte = (byte)myInt;
+
+double myDouble = (double)myByte;
+
+// bool myBool = (bool)myDouble;
+
+string myString = "false";
+
+// myBool = (bool)myString;
+
+// myString = (string)myInt;
+
+myString = myInt.ToString();
+
+// myBool = (bool)myByte;
+
+// myByte = (byte)myBool;
+
+short myShort = (short)myInt;
+
+char myChar = 'x';
+
+// myString = (string)myChar;
+
+long myLong = (long)myInt;
+
+decimal myDecimal = (decimal)myLong;
+myString = myString + myInt + myByte + myDouble + myChar;
+*/
+
+/*
+float myFloat = 7.0F;
+string chaos = "Chaos!";
+chaos = "Chaos " + myFloat;
+chaos = myFloat.ToString();
+Console.WriteLine(chaos);
+int newFloat = (int)myFloat;
+Console.WriteLine(Convert.ToString(newFloat, 2));
+*/
+
+/*
+using System;
+class Program
+{
+    public static void Main()
+    {
+        int done = MyMethod(false);
+        Console.WriteLine(done);
+        Console.ReadLine();
+    }
+    public static int MyMethod(bool add3)
+    {
+        int value = 12;
+
+        if (add3)
+            value += 3;
+        else
+            value -= 2;
+
+        return value;
+
+    }
+}
+*/
+
+static int MyMethod(bool add3)
+{
+    int value = 12;
+
+    if (add3)
+        value += 3;
+    else
+        value -= 2;
+
+    return value;
+
+}
+int done = MyMethod(false);
+Console.WriteLine(done);
+Console.ReadLine();
