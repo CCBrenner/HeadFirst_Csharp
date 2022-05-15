7bcfb740d219fc664cbc534fb7513fa9875f4d3f
Collyn Christopher Brenner <collynbrenner@gmail.com>
2022-05-14 20:15:37 -0500

    end of chapter 4, moving on to Unity Lab #2

---
+++ menuItemGeneratorUI/menuItemGeneratorUI/MainWindow.xaml.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace menuItemGeneratorUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            MakeTheMenu();
+        }
+
+        private void MakeTheMenu()
+        {
+            // This project uses DOM insertion (using WPF for a UI)
+            // It randomly selects items from a number of items by category,
+            // puts them into a string description, uses different lists for a specific catgory, and then
+            // uses an object simple for its number genreation for guac price
+            MenuItem[] menuItems = new MenuItem[5];
+            string guacamolePrice;
+
+            for (int i = 0; i < 5; i++)
+            {
+                menuItems[i] = new MenuItem();
+                if (i >= 3)
+                {
+                    menuItems[i].Breads = new string[]
+                    {
+                        "plain bagel", "onion bagel", "pumpernickle bagel", "everything bagel"
+                    };
+                }
+                menuItems[i].Generate();
+            }
+
+            // DOM Insertion (items 1 - 5)
+            item1.Text = menuItems[0].Description;
+            price1.Text = menuItems[0].Price;
+            item2.Text = menuItems[1].Description;
+            price2.Text = menuItems[1].Price;
+            item3.Text = menuItems[2].Description;
+            price3.Text = menuItems[2].Price;
+            item4.Text = menuItems[3].Description;
+            price4.Text = menuItems[3].Price;
+            item5.Text = menuItems[4].Description;
+            price5.Text = menuItems[4].Price;
+
+            // 6th Item - uses different items by category
+            MenuItem specialMenuItem = new MenuItem()
+            {
+                Proteins = new string[] { "Organic ham", "Mushroom patty", "Mortadella" },
+                Condiments = new string[] { "a gluten-free roll", "a wrap", "pita" },
+                Breads = new string[] { "dijon mustard", "miso dressing", "au jus" }
+            };
+            specialMenuItem.Generate();
+
+            // DOM Insertion for 6th item
+            item6.Text = specialMenuItem.Description;
+            price6.Text = specialMenuItem.Price;
+
+            // The guac price
+            MenuItem guacamoleMenuItem = new MenuItem();
+            guacamoleMenuItem.Generate();
+            guacamolePrice = guacamoleMenuItem.Price;
+
+            guacamole.Text = "Add guacamole for " + guacamolePrice;
+
+
+
+        }
+    }
+
+    class MenuItem
+    {
+        // 'static' here makes Random() sequence shared among objects, allowing for the Next() number in the sequence to be pulled instead of the first index of a new Random() object created from the same seed (which is based on system clock and likely will not change fast enough to provide a different set of random numbers in a sequence
+        public static Random Randomizer = new Random();
+
+        public string[] Proteins = { "Roast beef", "Salami", "Turkey", "Ham", "Pastrami", "Tofu" };
+        public string[] Condiments = { "yellow mustard", "brown mustard", "honey mustard", "mayo", "relish", "french dressing" };
+        public string[] Breads = { "rye", "white", "wheat", "pumpernickel", "a roll" };
+
+        public string Description = "";
+        public string Price;
+
+        public void Generate()
+        {
+            string randomProtein = Proteins[Randomizer.Next(Proteins.Length)];
+            string randomCondiment = Condiments[Randomizer.Next(Condiments.Length)];
+            string randomBread = Breads[Randomizer.Next(Breads.Length)];
+
+            decimal bucks = Randomizer.Next(2, 5);
+            decimal cents = Randomizer.Next(1, 98);
+            decimal price = bucks + (cents * .01M);
+            Price = price.ToString("c");
+            Description = randomBread + " with " + randomProtein + " and " + randomCondiment;
+        }
+    }
+}
---
+++ randomNumberGeneratorExhibition/randomNumberGeneratorExhibition/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+
+class Program
+{
+    public static void Main()
+    {
+        Random random = new Random();
+        int randomInt = random.Next();
+        Console.WriteLine(randomInt.ToString());
+
+        int zeroToNine = random.Next(10);
+        Console.WriteLine(zeroToNine);
+
+        int dieRoll = random.Next(1, 7);
+        Console.WriteLine(dieRoll);
+
+        double randomDouble = random.NextDouble();
+        Console.WriteLine(randomDouble);
+        Console.WriteLine(randomDouble * 100);
+        Console.WriteLine((float)randomDouble * 100F);
+        Console.WriteLine((decimal)randomDouble * 100M);
+
+        int zeroToOne = random.Next(2);
+        bool coinToss = Convert.ToBoolean(zeroToOne);
+        Console.WriteLine(coinToss);
+    }
+}
