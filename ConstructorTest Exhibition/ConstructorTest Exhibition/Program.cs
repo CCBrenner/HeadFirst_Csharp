d838647f07afdf399a426ad41fac7dc9396ccded
Collyn Christopher Brenner <collynbrenner@gmail.com>
2022-05-21 11:40:30 -0500

    completed pool puzzle of CH 5

---
+++ ConstructorTest Exhibition/ConstructorTest Exhibition/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class ConstructorTest
+{
+    public int i = 1;
+
+    // Parameterless constructor
+    public ConstructorTest()
+    {
+        Console.WriteLine($"i is {i}.");
+    }
+
+    public static void Main(string[] args)
+    {
+        ConstructorTest constructorTest = new ConstructorTest();
+    }
+}
--- PaintballGun Encapsulation Exhibition/PaintballGun Encapsulation Exhibition/Program.cs
+++ PaintballGun Encapsulation Exhibition/PaintballGun Encapsulation Exhibition/Program.cs
@@ -4,10 +4,22 @@ namespace PaintballGun
 {
     class PaintballGun
     {
-        public const int MAGAZINE_SIZE = 16;
+        public PaintballGun(int balls, int magazineSize, bool loaded)
+        {
+            // 'this' is used because the class already has a field called 'balls', providing
+            // the necessity for them to be differentiated
+            this.balls = balls;
+            MagazineSize = magazineSize;
+            if (!loaded) Reload();
+        }
 
         private int balls = 0;
-        private int ballsLoaded = 0;
+
+        // Auto-implemented property (same as Balls property below minus Reload(), but
+        // also 'private' making it a read-only property; if this were not marked 'private' for set then
+        // this class would not be considered "well-encapsulated" since outside code could change
+        // the instance's internal value with a reassignment)
+        public int MagazineSize { get; private set; }
 
         // Using a Property here; can be used to get and set the private field; PascalCase
         // for properties instead of camelCase which is used for fields
@@ -16,33 +28,26 @@ namespace PaintballGun
             get { return balls; }
             set
             {
-                if (value > 0)
-                {
-                    balls = value;
-                }
+                if (value > 0) balls = value;
                 Reload();
             }
         }
 
-        // Uses private *backing field* 'ballsLoaded'
-        public int BallsLoaded
-        {
-            get { return ballsLoaded; }
-            set { ballsLoaded = value; }
-        }
+        // Auto-implemented property; 'private' makes this into a read-only property, maintaining encapsulation
+        public int BallsLoaded { get; private set; }
 
-        public bool IsEmpty() { return ballsLoaded == 0; }
+        // Returns bool based on a conditional statement
+        public bool IsEmpty() { return BallsLoaded == 0; }
 
-        
         public void Reload()
         {
-            if (balls > MAGAZINE_SIZE) ballsLoaded = MAGAZINE_SIZE;
-            else ballsLoaded = balls;
+            if (balls > MagazineSize) BallsLoaded = MagazineSize;
+            else BallsLoaded = balls;
         }
         public bool Shoot()
         {
-            if (ballsLoaded == 0) return false;
-            ballsLoaded--;
+            if (BallsLoaded == 0) return false;
+            BallsLoaded--;
             balls--;
             return true;
         }
@@ -52,7 +57,13 @@ namespace PaintballGun
     {
         static void Main(string[] args)
         {
-            PaintballGun gun = new PaintballGun();
+            int numberOfBalls = ReadInt(20, "Number of Balls");
+            int magazineSize = ReadInt(16, "Magazine size");
+
+            Console.Write($"Loaded [false]: ");
+            bool.TryParse(Console.ReadLine(), out bool isLoaded);
+
+            PaintballGun gun = new PaintballGun(numberOfBalls, magazineSize, isLoaded);
 
             while (true)
             {
@@ -64,8 +75,35 @@ namespace PaintballGun
                 else if (key == 'r') gun.Reload();
                 // This assignment uses the 'set' method of the property 'Balls' to
                 // update the private field via the (public) Property
-                else if (key == '+') gun.Balls += PaintballGun.MAGAZINE_SIZE;
+                else if (key == '+') gun.Balls += gun.MagazineSize;
                 else if (key == 'q') return;
+            }
+        }
+
+        /// <summary>
+        /// Writes a prompt and reads an int value from the console.
+        /// </summary>
+        /// <param name="lastUsedValue">The default value.</param>
+        /// <param name="prompt">Prompt to print to the console.</param>
+        /// <returns>The int value read, or the default value if unable to parse.</returns>
+        static int ReadInt(int lastUsedValue, string prompt)
+        {
+            // Write the prompt followed by [default value]:
+            Console.WriteLine(prompt + " [" + lastUsedValue + "] ");
+
+            // Read the line from the input and use int.TryParse to attempt to parse it
+            string userInput = Console.ReadLine();
+            if (int.TryParse(userInput, out int value))
+            {
+                // If it can be parsed, write " using value " + value to the console
+                Console.WriteLine(" using value " + value);
+                return value;
+            }
+            else
+            {
+                // Otherwise write " using default value" + lastUsedValue to the console
+                Console.WriteLine(" using default value.");
+                return lastUsedValue;
             }
         }
     }
