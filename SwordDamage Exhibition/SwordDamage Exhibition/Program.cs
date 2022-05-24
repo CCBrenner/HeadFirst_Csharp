d8fd52c8a918f97c5c8385a7339667a580d53788
Collyn Christopher Brenner <collynbrenner@gmail.com>
2022-05-24 04:14:18 -0500

    going back and fisxing SwordDamage Exhibition from Ch 5 to move forward in Ch 6

--- SwordDamage Exhibition/SwordDamage Exhibition/Program.cs
+++ SwordDamage Exhibition/SwordDamage Exhibition/Program.cs
@@ -7,16 +7,16 @@ namespace SwordDamage
         public const int BASE_DAMAGE = 3;
         public const int FLAME_DAMAGE = 2;
 
-        public int Roll;
+        public int Roll { get; set; }
         // *Made private for purposes of improving encapsulation
         private decimal magicMultiplier = 1M;
         private int flamingDamage = 0;
         public int Damage;
 
-        private void CalculateDamage()
+        private void UpdateDamage()
         {
             Damage = BASE_DAMAGE + (int)(Roll * magicMultiplier) + flamingDamage;
-            Debug.WriteLine($"CalculateDamage finished: {Damage} (roll: {Roll})");
+            // Debug.WriteLine($"CalculateDamage finished: {Damage} (roll: {Roll})");
         }
 
         public void SetMagic(bool magical)
@@ -29,18 +29,50 @@ namespace SwordDamage
             {
                 magicMultiplier = 1M;
             }
-            CalculateDamage();
-            Debug.WriteLine($"SetFlaming finished: {Damage} (roll: {Roll})");
+            UpdateDamage();
+            // Debug.WriteLine($"SetFlaming finished: {Damage} (roll: {Roll})");
         }
 
         public void SetFlaming(bool flaming)
         {
-            CalculateDamage();
+            UpdateDamage();
             if (flaming)
             {
                 Damage += FLAME_DAMAGE;
             }
-            Debug.WriteLine($"SetFlaming finished: {Damage} (roll: {Roll})");
+            // Debug.WriteLine($"SetFlaming finished: {Damage} (roll: {Roll})");
+        }
+    }
+
+    class Program
+    {
+        static Random random = new Random();
+        SwordDamage swordDamage = new SwordDamage();
+        public void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("0 for no magic/flaming, 1 for magic, 2 for flaming, 3 for both, any other key to quit: ");
+                char userChoice = Console.ReadKey().KeyChar;
+                if (userChoice != '0' && userChoice != '1' && userChoice != '2' && userChoice != '3') return;
+                if (userChoice == '1' || userChoice == '3')
+                {
+                    swordDamage.SetMagic(true);
+                }
+                if (userChoice == '2' || userChoice == '3')
+                {
+                    swordDamage.SetFlaming(true);
+                }
+                DisplayDamage();
+            }
+        }
+        public void RollDice()
+        { 
+        swordDamage.Roll = random.Next(1, 7) + random.Next(1, 7) + random.Next(1, 7);
+        }
+        public void DisplayDamage()
+        {
+            Console.WriteLine($"Roll 3d6 is { swordDamage.Roll } for { swordDamage.Damage } HP.");
         }
     }
 }
