4ec7d8cd326e4373df92cd8f2987785fc121bd45
Collyn Christopher Brenner <collynbrenner@gmail.com>
2022-05-26 19:21:46 -0500

    ANimalFeedVendingMachineExercise added; protected keyword and overrides

---
+++ AnimalFeedVendingMachineExercise/AnimalFeedVendingMachineExercise/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class VendingMachine
+{
+    public virtual string Item { get; }
+    protected virtual bool CheckAmount(decimal money)
+    {
+        return false;
+    }
+    public string Dispense(decimal money)
+    {
+        if (CheckAmount(money)) return Item;
+        else return "Please enter the right amount.";
+    }
+}
+
+class AnimalFeedVendingMachine : VendingMachine
+{
+    public override string Item
+    {
+        get { return "a handful of feed"; }
+    }
+    protected override bool CheckAmount(decimal money)
+    {
+        return money >= 1.25M;
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        VendingMachine vendingMachine = new AnimalFeedVendingMachine();
+        Console.WriteLine(vendingMachine.Dispense(2.00M));
+        // vendingMachine.CheckAmount(1F);  // won't compile because the method is protected, only the subclass can access it
+    }
+}
