4b97cc0068c2047f905f0afed3a638b56757be22
Collyn Christopher Brenner <collynbrenner@gmail.com>
2022-06-04 15:22:27 -0500

    added HoneyVault class to BeehiveManagementSystem project

--- BeehiveManagementSystem/BeehiveManagementSystem/MainWindow.xaml.cs
+++ BeehiveManagementSystem/BeehiveManagementSystem/MainWindow.xaml.cs
@@ -15,6 +15,41 @@ using System.Windows.Shapes;
 
 namespace BeehiveManagementSystem
 {
+    class HoneyVault
+    {
+        public HoneyVault()
+        {
+
+        }
+
+        public const float NECTAR_CONVERTION_RATIO = 0.19F;
+        public const float LOW_LEVEL_WARNING = 10F;
+
+
+        private float honey = 25F;
+        private float nectar = 100F;
+        private string statusReport;
+        public string StatusReport
+        {
+            get
+            {
+                statusReport = $"Amount of Honey in Vault: {honey}\nAmount of Nectar in Vault: {nectar}";
+                if (honey < LOW_LEVEL_WARNING) statusReport += "\nLOW HONEY - ADD A HONEY MANUFACTURER";
+                if (nectar < LOW_LEVEL_WARNING) statusReport += "\nLOW NECTAR - ADD A NECTAR COLLECTOR";
+                return statusReport;
+            }
+        }
+
+        public void CollectNectar(float nectarToStore) { nectar += nectarToStore > 0F ? nectarToStore : 0F; }
+        public void ConvertNectarToHoney(float amountToConvert)
+        {
+            amountToConvert = amountToConvert > nectar ? nectar : amountToConvert;
+            nectar -= amountToConvert;
+            honey += amountToConvert * NECTAR_CONVERTION_RATIO;
+        }
+        public bool ConsumeHoney(float consumptionAmount) { return consumptionAmount > honey; }
+    }
+
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
