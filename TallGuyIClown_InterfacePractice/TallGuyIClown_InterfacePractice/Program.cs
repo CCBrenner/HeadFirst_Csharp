ed3593db6a85f5db44a6d83c7db026a287cc01a7
Collyn Brenner <collynbrenner@gmail.com>
2022-06-20 12:32:58 -0500

    began learning about interfaces

---
+++ TallGuyIClown_InterfacePractice/TallGuyIClown_InterfacePractice/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class TallGuy
+{
+    public string Name;
+    public int Height;
+
+    public void TalkAboutYourself()
+    {
+        Console.WriteLine($"My name is {Name} and I'm {Height} inches tall.");
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        TallGuy tallguy = new TallGuy() { Height = 76, Name = "Jimmy" };
+        tallguy.TalkAboutYourself();
+    }
+}
