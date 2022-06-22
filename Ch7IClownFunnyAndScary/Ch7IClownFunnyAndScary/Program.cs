63186baa35dc22095b22bf9da21d355d01ff4ab1
Collyn Brenner <collynbrenner@gmail.com>
2022-06-22 12:39:15 -0500

    updated Ch7 scary clown interface project to include verbatim string literals and a default implementation

--- Ch7IClownFunnyAndScary/Ch7IClownFunnyAndScary/Program.cs
+++ Ch7IClownFunnyAndScary/Ch7IClownFunnyAndScary/Program.cs
@@ -4,13 +4,35 @@ interface IClown
 {
     protected static Random random = new Random();
     string FunnyThingIHave { get; }
+    private static int carCapacity = 12;
+    public static int CarCapacity
+    {
+        get { return carCapacity; }
+        set
+        {
+            if (value > 10) carCapacity = value;
+            else Console.Error.WriteLine($"Warning: Car capacity {value} is too small.");
+        }
+    }
     void Honk();
+    static string ClownCarDescription()
+    {
+        return $"A clown car with {random.Next(CarCapacity / 2, CarCapacity)} clowns.";
+    }
 }
 
 interface IScaryClown : IClown
 {
     string ScaryThingIHave { get; }
     void ScareLittleChildren();
+     void ScareAdults()
+    {
+        Console.WriteLine($@"I am an ancient evil that will haunt your dreams. 
+Behold my terrifying necklace with {random.Next(4, 10)} of my last victim's fingers.
+
+Oh, also, before I forget...");
+        ScareLittleChildren();
+    }
 }
 
 class FunnyFunny : IClown
@@ -46,11 +68,13 @@ class Program
 {
     public static void Main(string[] args)
     {
+        IClown.CarCapacity = 18;
+        Console.WriteLine(IClown.ClownCarDescription());
         IClown fingersTheClown = new ScaryScary("big red nose", 14);
         fingersTheClown.Honk();
         if (fingersTheClown is IScaryClown iScaryClownReference)
         {
-            iScaryClownReference.ScareLittleChildren();
+            iScaryClownReference.ScareAdults();
         }
     }
 }
