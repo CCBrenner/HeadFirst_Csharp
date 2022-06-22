ec769850839caa6ba9de567a8f7c7d4ad1a9508b
Collyn Christopher Brenner <collynbrenner@gmail.com>
2022-06-22 07:42:24 -0500

    stopping point

---
+++ Ch7IClownFunnyAndScary/Ch7IClownFunnyAndScary/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+interface IClown
+{
+    protected static Random random = new Random();
+    string FunnyThingIHave { get; }
+    void Honk();
+}
+
+interface IScaryClown : IClown
+{
+    string ScaryThingIHave { get; }
+    void ScareLittleChildren();
+}
+
+class FunnyFunny : IClown
+{
+    public FunnyFunny(string funnyThing)
+    {
+        funnyThingIHave = funnyThing;
+    }
+    private string funnyThingIHave;
+    public string FunnyThingIHave { get { return funnyThingIHave; } }
+
+    public void Honk()
+    {
+        Console.WriteLine($"Hi kids, I have a {funnyThingIHave}.");
+    }
+}
+
+class ScaryScary : FunnyFunny, IScaryClown
+{
+    public ScaryScary(string funnyThing, int scaryThingCount) : base(funnyThing)
+    {
+        ScaryThingIHave = $"{scaryThingCount} spiders";
+    }
+    public string ScaryThingIHave { get; set; }
+
+    public void ScareLittleChildren()
+    {
+        Console.WriteLine($"Boo! Gotcha! Look at my {ScaryThingIHave}");
+    }
+}
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        IClown fingersTheClown = new ScaryScary("big red nose", 14);
+        fingersTheClown.Honk();
+        if (fingersTheClown is IScaryClown iScaryClownReference)
+        {
+            iScaryClownReference.ScareLittleChildren();
+        }
+    }
+}
