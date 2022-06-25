517e9eb8e3cfaca4b81e4fe8964d3232d83a1bf8
Collyn Christopher Brenner <collynbrenner@gmail.com>
2022-06-25 13:42:04 -0500

    done with Ch8 ducks example for now, learned how to sort by one and then another property

--- Ch8DucksB/Ch8DucksB/Program.cs
+++ Ch8DucksB/Ch8DucksB/Program.cs
@@ -29,6 +29,52 @@ class Duck : IComparable<Duck>
     }
 }
 
+enum SortCriteria
+{
+    SizeThenKind,
+    KindThenSize,
+}
+
+class DuckComparer : IComparer<Duck>
+{
+    public SortCriteria SortBy = SortCriteria.SizeThenKind;
+    public int Compare(Duck x, Duck y)
+    {
+        if (SortBy == SortCriteria.KindThenSize)
+        {
+            if (x.Kind > y.Kind)
+                return 1;
+            else if (x.Kind < y.Kind)
+                return -1;
+            else
+            {
+                if (x.Size > y.Size)
+                    return 1;
+                else if (x.Size < y.Size)
+                    return -1;
+                else
+                    return 0;
+            }
+        }
+        else
+        {
+            if (x.Size > y.Size)
+                return 1;
+            else if (x.Size < y.Size)
+                return -1;
+            else
+            {
+                if (x.Kind > y.Kind)
+                    return 1;
+                else if (x.Kind < y.Kind)
+                    return -1;
+                else
+                    return 0;
+            }
+        }
+    }
+}
+
 class DuckComparerBySize : IComparer<Duck>
 {
     public int Compare(Duck x, Duck y)
@@ -37,14 +83,15 @@ class DuckComparerBySize : IComparer<Duck>
         {
             return 1;
         }
-        else if (x.Size < y.Size) {
+        else if (x.Size < y.Size)
+        {
             return -1;
-        } 
+        }
         else
         {
             return 0;
         }
-        
+
     }
 }
 
@@ -80,6 +127,7 @@ class Program
             new Duck() { Kind = KindOfDuck.Loon, Size = 13 },
         };
 
+
         /* 
         This uses IComparable with CompareTo() method defined in the Duck class
         (can be thought of as the Duck class' default ordering to sort by) 
@@ -87,11 +135,23 @@ class Program
         // ducks.Sort();  
         /* 
         IComparer object can be passed to List<Duck>.Sort() due to the method being overloaded.
-
         */
+        /*
         IComparer<Duck> ducksBySize = new DuckComparerBySize();
         IComparer<Duck> ducksByKind = new DuckComparerByKind();
         ducks.Sort(ducksByKind);
+        PrintDucks(ducks);
+        */
+
+        DuckComparer comparer = new DuckComparer();
+        Console.WriteLine("Sorting by Size then Kind: ");
+        comparer.SortBy = SortCriteria.SizeThenKind;
+        ducks.Sort(comparer);
+        PrintDucks(ducks);
+        Console.WriteLine();
+        Console.WriteLine("Sorting by Kind then Size: ");
+        comparer.SortBy = SortCriteria.KindThenSize;
+        ducks.Sort(comparer);
         PrintDucks(ducks);
     }
     public static void PrintDucks(List<Duck> ducks)
