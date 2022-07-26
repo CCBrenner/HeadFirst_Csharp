cfa759d4a64e6f57f79a0e31884269a825d02a51
Collyn Brenner <collynbrenner@gmail.com>
2022-07-26 18:49:45 -0500

    Stopping point

---
+++ Ch9DeferredEvaluationWithLinq/Ch9DeferredEvaluationWithLinq/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+class PrintWhenGetting
+{
+    private int instanceNumber;
+    public int InstanceNumber
+    {
+        set { instanceNumber = value; }
+        get
+        {
+            Console.WriteLine($"Getting #{instanceNumber}");
+            return instanceNumber;
+        }
+    }
+}
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        var listOfObjects = new List<PrintWhenGetting>();
+        for (int i = 1; i < 5; i++)
+            listOfObjects.Add(new PrintWhenGetting() { InstanceNumber = i });
+
+        Console.WriteLine("Set up the query:");
+        var result =
+            from o in listOfObjects
+            select o.InstanceNumber;
+
+        Console.WriteLine("Run the foreach");
+        foreach (var number in listOfObjects)
+            Console.WriteLine($"Writing #{number}");
+    }
+}
