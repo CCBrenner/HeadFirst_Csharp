bfb86693d55375f7762de51fcf732914c6bf32e6
Collyn Christopher Brenner <collynbrenner@gmail.com>
2022-07-26 07:47:07 -0500

    updated Ch9ComicsCollectionLinq project to create a string sequence instead of a Comics sequence in the Main method

---
+++ Ch9ComicsCollectionLinq/Ch9ComicsCollectionLinq/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        // This used to be a sequence of Comics. Now it is a sequence of strings.
+        IEnumerable<string> mostExpensive =
+            from comic in Comic.Catelog
+            where Comic.Prices[comic.Issue] > 500
+            // orderby -Comic.Prices[comic.Issue]
+            orderby Comic.Prices[comic.Issue] descending  // 'descending' keyword replaces the minus sign here so that the claus is more readable/understandable
+            select $"{comic} is worth {Comic.Prices[comic.Issue]:c}";
+
+        foreach (string comic in mostExpensive)
+            Console.WriteLine(comic);
+    }
+}
