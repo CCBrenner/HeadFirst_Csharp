32bf201fc1628010f3846723c3e7657b00b0cf94
Collyn Christopher Brenner <collynbrenner@gmail.com>
2022-08-03 19:31:21 -0500

    additional solution added

---
+++ Ch9PowerOfTwoYieldReturnEnumerator/Ch9PowerOfTwoYieldReturnEnumerator/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+
+class PowerOfTwo : IEnumerable<int>
+{
+    public IEnumerator<int> GetEnumerator()
+    {
+        for (int i = 2; i > 0; i = (int)Math.Pow(i, 2))
+        {
+            yield return i;
+        }
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+}
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        var sequence = new PowerOfTwo();
+        foreach (var element in sequence)
+        {
+            Console.Write($"{element} ");
+        };
+        Console.WriteLine($"\n\n{Math.Round(Math.Log(int.MaxValue, 2))}");
+    }
+}
