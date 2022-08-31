64efca87217106053345cb2c4c428147cbfa2049
Collyn Christopher Brenner <collynbrenner@gmail.com>
2022-08-31 13:13:17 -0500

    added Ch10MagnetPuzzleBlobbo to Ch10Swindler solution

---
+++ Ch10Swindler/Ch10MagnetPuzzleBlobbo/Program.cs
@@ -0,0 +1,48 @@
+﻿using System.IO;
+
+namespace Ch10MagnetPuzzleBlobbo
+{
+    class Flobbo
+    {
+        public Flobbo(string zap)
+        {
+            this.zap = zap;
+        }
+        private string zap;
+        public StreamWriter Snobbo()
+        {
+            return new StreamWriter("macaw.txt");
+        }
+        public bool Blobbo(StreamWriter sw)
+        {
+            sw.WriteLine(zap);
+            zap = "green purple";
+            return false;
+        }
+        public bool Blobbo(bool Already, StreamWriter sw)
+        {
+            if (Already)
+            {
+                sw.WriteLine(zap);
+                sw.Close();
+                return false;
+            }
+            else
+            {
+                sw.WriteLine(zap);
+                zap = "red orange";
+                return true;
+            }
+        }
+    }
+
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Flobbo f = new Flobbo("blue yellow");
+            StreamWriter sw = f.Snobbo();
+            f.Blobbo(f.Blobbo(f.Blobbo(sw), sw), sw);
+        }
+    }
+}
