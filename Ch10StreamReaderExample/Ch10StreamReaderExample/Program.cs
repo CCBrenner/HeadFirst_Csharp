bf0984074a9c4c2234d2e40997121d6839341e9a
Collyn Christopher Brenner <collynbrenner@gmail.com>
2022-08-31 18:31:43 -0500

    Captain Amazing used StreamWriter and StreamReader to decipher the Swindler's stored file message

---
+++ Ch10StreamReaderExample/Ch10StreamReaderExample/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.IO;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        var folder = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+
+        // Put "secret_plan.txt" file in Windows Documents folder to be read
+        var reader = new StreamReader($"{folder}{Path.DirectorySeparatorChar}secret_plan.txt");
+        var writer = new StreamWriter($"{folder}{Path.DirectorySeparatorChar}emailToCaptainA.txt");
+
+        writer.WriteLine("To: CaptainAmazing@objectville.net");
+        writer.WriteLine("From: Commissioner@objectville.net");
+        writer.WriteLine("Subject: Can you save the day . . . again?");
+        writer.WriteLine();
+        writer.WriteLine("We've discovered the Swindler's terrible plan:");
+
+        while (!reader.EndOfStream)
+        {
+            var lineFromPlan = reader.ReadLine();
+            writer.WriteLine($"The plan -> {lineFromPlan}");
+        }
+        writer.WriteLine();
+        writer.WriteLine("Can you help us?");
+
+        writer.Close();
+        reader.Close();
+    }
+}
