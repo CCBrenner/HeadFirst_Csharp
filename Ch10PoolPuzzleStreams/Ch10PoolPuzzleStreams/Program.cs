2aabb8519554240400cd4a92e19ba354d9abdf63
Collyn Christopher Brenner <collynbrenner@gmail.com>
2022-08-31 18:50:47 -0500

    stopping point

---
+++ Ch10PoolPuzzleStreams/Ch10PoolPuzzleStreams/Program.cs
@@ -0,0 +1,57 @@
+﻿using System.IO;
+
+class Pineapple
+{
+    const ______ __d = "delivery.txt";
+    public _____ __
+    {
+        North,
+        South,
+        East,
+        West,
+        Flamingo,
+    }
+    public static void Main(string[] args)
+    {
+        StreamWriter o = new StreamWriter("order.txt");
+        var pz = new Party(new StreamWriter(d, true));
+        pz.____(Fargo.Flamingo);
+        for (____w = 3; w >= 0; w--)
+        {
+            var i = new ______(new ______(d, false));
+            i.Idaho((Fargo)w);
+            Party.p = new _____(new ______(d));
+            pz.HowMuch(o);
+        }
+        o.WriteLine("That's all folks!");
+        o.Close();
+    }
+}
+
+class Pizza
+{
+    private StreamWriter writer;
+    public Pizza(StreamWriter writer)
+    {
+        this.writer = writer;
+    }
+    public void Idaho(_____ ____.Fargo f)
+    {
+        writer.____(f);
+        writer._____();
+    }
+}
+
+class Party
+{
+    private StreamReader reader;
+    public Party(StreamReader reader)
+    {
+        this.reader = reader;
+    }
+    public void HowMuch(________ q)
+    {
+        q.____(reader.______());
+        reader._______();
+    }
+}
