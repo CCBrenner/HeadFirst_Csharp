afa99eb0134c21a8d905c1a728b0a312fd120547
Collyn Christopher Brenner <collynbrenner@gmail.com>
2022-09-10 18:26:09 -0500

    added GameController.ParseInput() test

--- Ch10HideAndSeekEndOfChapterProj/Ch10HideAndSeekEndOfChapterProjTest/GameControllerTests.cs
+++ Ch10HideAndSeekEndOfChapterProj/Ch10HideAndSeekEndOfChapterProjTest/GameControllerTests.cs
@@ -35,11 +35,52 @@ namespace Ch10HideAndSeekEndOfChapterProjTest
             Assert.IsTrue(gameController.Move(Direction.Southeast));
             Assert.AreEqual("Landing", gameController.CurrentLocation.Name);
         }
-        /*
+        [TestMethod]
         public void TestParseInput()
         {
-            throw new NotImplementedException();
+            string initialStatus = gameController.Status;
+
+            Assert.AreEqual("That's not a valid direction.", gameController.ParseInput("x"));
+            Assert.AreEqual(initialStatus, gameController.Status);
+
+            Assert.AreEqual("There's no exit in that direction.", gameController.ParseInput("Up"));
+            Assert.AreEqual(initialStatus, gameController.Status);
+
+            Assert.AreEqual("Moving East", gameController.ParseInput("East"));
+            Assert.AreEqual($"You are in the Hallway. You see the following exits:" +
+                $"{Environment.NewLine} - the Donwstairs Bathroom is to the North" +
+                $"{Environment.NewLine} - the Living Room is to the South" +
+                $"{Environment.NewLine} - the Entry is to the West" +
+                $"{Environment.NewLine} - the Kitchen is to the Northwest" +
+                $"{Environment.NewLine} - the Landing is Up", gameController.Status);
+
+            Assert.AreEqual("Moving South", gameController.ParseInput("south"));
+            Assert.AreEqual($"You are in the Hallway. You see the following exits:" +
+                $"{Environment.NewLine} - the Hallway is to the North", gameController.Status);
+
+            Assert.AreEqual("Moving North", gameController.ParseInput("north"));
+            Assert.AreEqual("Moving Up", gameController.ParseInput("UP"));
+            Assert.AreEqual($"You are on the Landing. You see the following exits:" +
+                $"{Environment.NewLine} - the Pantry is to the South" +
+                $"{Environment.NewLine} - the Upstairs Bathroom is to the West" +
+                $"{Environment.NewLine} - the Nursery is to the Southwest" +
+                $"{Environment.NewLine} - the Master Bedroom is to the Northwest" +
+                $"{Environment.NewLine} - the Kids Room is to the Southeast" +
+                $"{Environment.NewLine} - the Hallway is Down", gameController.Status);
+
+            Assert.AreEqual("That's not a valid direction.", gameController.ParseInput("Master dRoom"));
+            Assert.AreEqual("There's no exit in that direction.", gameController.ParseInput("NOrth"));
+            Assert.AreEqual($"You are on the Landing. You see the following exits:" +
+                $"{Environment.NewLine} - the Pantry is to the South" +
+                $"{Environment.NewLine} - the Upstairs Bathroom is to the West" +
+                $"{Environment.NewLine} - the Nursery is to the Southwest" +
+                $"{Environment.NewLine} - the Master Bedroom is to the Northwest" +
+                $"{Environment.NewLine} - the Kids Room is to the Southeast" +
+                $"{Environment.NewLine} - the Hallway is Down", gameController.Status);
+            Assert.AreEqual("Moving Northwest", gameController.ParseInput("Master BedRoom"));
+            Assert.AreEqual($"You are in the Master Bedroom. You see the following exits:" +
+                $"{Environment.NewLine} - the Landing is to the Southeast" +
+                $"{Environment.NewLine} - the Master Bathroom is to the East", gameController.Status);
         }
-        */
     }
 }
