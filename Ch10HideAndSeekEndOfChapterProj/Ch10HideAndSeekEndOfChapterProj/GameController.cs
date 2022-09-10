1a929cb08430898bf57bd5d53347e4e9b10a7638
Collyn Christopher Brenner <collynbrenner@gmail.com>
2022-09-10 17:44:41 -0500

    added GameController class and test class, also added first failing test

---
+++ Ch10HideAndSeekEndOfChapterProj/Ch10HideAndSeekEndOfChapterProj/GameController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch10HideAndSeekEndOfChapterProj
+{
+    public class GameController
+    {
+        public GameController() => CurrentLocation = House.Entry;
+        public Location CurrentLocation { get; private set; }
+        public string Status => throw new NotImplementedException();
+        public string Prompt => "Which direction do you want to go: ";
+        public bool Move(Direction exitDirection)
+        {
+            throw new NotImplementedException();
+        }
+        public string ParseInput(string input)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Ch10HideAndSeekEndOfChapterProj/Ch10HideAndSeekEndOfChapterProjTest/GameControllerTests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Ch10HideAndSeekEndOfChapterProjTest
+{
+    using Ch10HideAndSeekEndOfChapterProj;
+    [TestClass]
+    public class GameControllerTests
+    {
+        GameController gameController;
+
+        [TestInitialize]
+        public void Initialize() => gameController = new GameController();
+        [TestMethod]
+        public void TestMovement()
+        {
+            Assert.AreEqual("Entry", gameController.CurrentLocation.Name);
+            Assert.IsFalse(gameController.Move(Direction.Up));
+
+            Assert.IsTrue(gameController.Move(Direction.East));
+            Assert.AreEqual("Hallway", gameController.CurrentLocation.Name);
+
+            Assert.IsTrue(gameController.Move(Direction.Up));
+            Assert.AreEqual("Landing", gameController.CurrentLocation.Name);
+
+            Assert.IsFalse(gameController.Move(Direction.North));
+            Assert.AreEqual("Landing", gameController.CurrentLocation.Name);
+
+            Assert.IsTrue(gameController.Move(Direction.Northwest));
+            Assert.AreEqual("Master Bedroom", gameController.CurrentLocation.Name);
+
+            Assert.IsFalse(gameController.Move(Direction.North));
+            Assert.AreEqual("Master Bedroom", gameController.CurrentLocation.Name);
+
+            Assert.IsTrue(gameController.Move(Direction.Southeast));
+            Assert.AreEqual("Landing", gameController.CurrentLocation.Name);
+        }
+        /*
+        public void TestParseInput()
+        {
+            throw new NotImplementedException();
+        }
+        */
+    }
+}
