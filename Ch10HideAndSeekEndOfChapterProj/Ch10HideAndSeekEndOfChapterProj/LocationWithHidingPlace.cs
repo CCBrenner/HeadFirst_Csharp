a644370f68fcfa9b8955a2d54448411a43fcaed4
Collyn Christopher Brenner <collynbrenner@gmail.com>
2022-09-13 13:33:24 -0500

    added Opponent and LocationWithHidingPlace classes plus LocationWithHidingPlaceTests only unittest

---
+++ Ch10HideAndSeekEndOfChapterProj/Ch10HideAndSeekEndOfChapterProj/LocationWithHidingPlace.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Ch10HideAndSeekEndOfChapterProj
+{
+    public class LocationWithHidingPlace : Location
+    {
+        public LocationWithHidingPlace(string name, string hidingPlace) : base(name)
+        {
+            throw new NotImplementedException();
+        }
+        public string HidingPlace => throw new NotImplementedException();
+        public void Hide(Opponent opponent)
+        {
+            throw new NotImplementedException();
+        }
+        public IEnumerable<Opponent> CheckHidingPlace()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Ch10HideAndSeekEndOfChapterProj/Ch10HideAndSeekEndOfChapterProj/Opponent.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Ch10HideAndSeekEndOfChapterProj
+{
+    public class Opponent
+    {
+        public Opponent(string name) => name = name;
+        public readonly string Name;
+        public override string ToString() => Name;
+        public void Hide()
+        {
+            // at some point calls a LocationWit5hHidingPlace.Hide() method with this Opponent as argument
+            throw new NotImplementedException();
+        }
+    }
+}
