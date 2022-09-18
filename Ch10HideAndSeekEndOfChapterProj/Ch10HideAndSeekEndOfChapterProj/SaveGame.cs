70d4c62c69372497287bb1995f8936686757655a
Collyn Christopher Brenner <collynbrenner@gmail.com>
2022-09-17 19:13:44 -0500

    added SaveGame class and unittest intializer and first test for saving and resuming game with file save and load

---
+++ Ch10HideAndSeekEndOfChapterProj/Ch10HideAndSeekEndOfChapterProj/SaveGame.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Ch10HideAndSeekEndOfChapterProj
+{
+    public class SaveGame
+    {
+        public SaveGame()
+        {
+            throw new NotImplementedException();
+        }
+        public int MoveNumber { get; private set; }
+        public string Status { get; private set; }
+        /// <summary>
+        /// Opponents and their hiding locations
+        /// </summary>
+        /// <key>Opponent in hiding</key>
+        /// <value>Opponent's hiding location</value>
+        public Dictionary<string, string> opponentsInHidingLocations { get; private set; }
+        public List<Opponent> foundOpponents { get; private set; }
+    }
+}
