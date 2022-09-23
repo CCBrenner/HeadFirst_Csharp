db0766eaedb2302e9566e57648877ce296e22497
Collyn Christopher Brenner <collynbrenner@gmail.com>
2022-09-23 17:49:46 -0500

    refactored save and load methods of GameController to be partially in SaveGame class for its respective procedures having to do with its own class

--- Ch10HideAndSeekEndOfChapterProj/Ch10HideAndSeekEndOfChapterProj/GameController.cs
+++ Ch10HideAndSeekEndOfChapterProj/Ch10HideAndSeekEndOfChapterProj/GameController.cs
@@ -18,19 +18,19 @@ namespace Ch10HideAndSeekEndOfChapterProj
 
             CurrentLocation = House.Entry;
         }
-        private List<Opponent> foundOpponents = new List<Opponent>();
+        public List<Opponent> FoundOpponents = new List<Opponent>();
         public Location CurrentLocation { get; private set; }
         private IEnumerable<Location> locationHubs { get; }
         public int MoveNumber { get; set; } = 1;
         public string Prompt => $"{MoveNumber}: Which direction do you want to go (or type 'check'): ";
-        public bool GameOver => Opponents.Count() == foundOpponents.Count();
+        public bool GameOver => Opponents.Count() == FoundOpponents.Count();
         private string status;
         public string Status => $"You are {HandleLandingGrammar(CurrentLocation.Name)} the {CurrentLocation.Name}. " +
             $"You see the following exits:" +
             $"{Environment.NewLine}" +
             $"{string.Join($"{Environment.NewLine}", CurrentLocation.ExitList)}" +
             $"{MentionHidingPlace(CurrentLocation)}" +
-            $"{Environment.NewLine}You have found {foundOpponents.Count()} of {Opponents.Count()} opponents: {string.Join(", ", foundOpponents)}"; 
+            $"{Environment.NewLine}You have found {FoundOpponents.Count()} of {Opponents.Count()} opponents: {string.Join(", ", FoundOpponents)}"; 
         public IEnumerable<Opponent> Opponents = new List<Opponent>()
         {
             new Opponent("Joe"),
@@ -48,81 +48,26 @@ namespace Ch10HideAndSeekEndOfChapterProj
         }
         private string Save(string nameForSavedFile)
         {
-            Dictionary<string, string> opponentsInHidingLocations = new Dictionary<string, string>();
-            foreach (Location location in House.Locations)
-                if ((location as LocationWithHidingPlace).HidingPlace != "")
-                    foreach (Opponent opponent in (location as LocationWithHidingPlace).OpponentsHiddenHere)
-                        opponentsInHidingLocations.Add(opponent.Name, location.Name);
-            string currentLocation = CurrentLocation.Name;
-            int moveNumber = MoveNumber;
-            string status = Status;
-            List<string> foundOpponents = new List<string>();
-            foreach (Opponent opponent in this.foundOpponents) foundOpponents.Add(opponent.Name);
-
-            SaveGame saveGame = new SaveGame()
-            {
-                OpponentsInHidingLocations = opponentsInHidingLocations,
-                FoundOpponents = foundOpponents,
-                CurrentLocationName = currentLocation,
-                MoveNumber = moveNumber,
-                Status = status,
-            };
-
-            // string savedGame = JsonSerializer.Serialize(saveGame);  // Wasn't serializing the Opponent objects for some reason
-            string savedGame = JsonConvert.SerializeObject(saveGame);  // Had to use this method from the Newtonsoft.Json NuGet package
-
-            if (!nameForSavedFile.Contains('\\') && !nameForSavedFile.Contains(' '))
-            {
-                var folder = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
-                using (StreamWriter writer = new StreamWriter($"{folder}{Path.DirectorySeparatorChar}{nameForSavedFile}.json", false))
-                    writer.WriteLine(savedGame);
-                return $"Saved current game to {nameForSavedFile}.json";
-            }
-            return $"Could not save game to {nameForSavedFile}.json";
+            SaveGame saveGame = new SaveGame();
+            return saveGame.Save(this, nameForSavedFile);
         }
         private string Load(string nameOfFileToLoad)
         {
-            nameOfFileToLoad = nameOfFileToLoad.Split('.')[0];  // if loaded with extension attached to filename, take only the filename
-            if (nameOfFileToLoad.Contains('\\') || nameOfFileToLoad.Contains(' '))
-                return "Could not load game: Invalid characters detected. Please remove backslashes and/or spaces from file name.";
-
-            string gameDataToDeserialize = "";
-            try
-            {
-                // an alternative to the line below it:
-                // string filePath = $"{Environment.GetFolderPath(Environment.SpecialFolder.Personal)}{Path.DirectorySeparatorChar}{nameOfFileToLoad}.json"))
-                string filePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Personal), $"{nameOfFileToLoad}.json");
-                using (StreamReader reader = new StreamReader(filePath))
-                {
-                    gameDataToDeserialize = reader.ReadToEnd();
-                }
-                File.Delete(filePath);
-            }
-            catch
-            {
-                return $"Could not load game: Saved file of name \"{nameOfFileToLoad}.json\" not found";
-            }
-
-            SaveGame? loadedGame = new SaveGame();
-            try
-            {
-                loadedGame = System.Text.Json.JsonSerializer.Deserialize<SaveGame>(gameDataToDeserialize);
-            }
-            catch
-            {
-                return $"Could not load game: Could not deserialize file with name \"{nameOfFileToLoad}.json\"";
-            }
-
-            foreach (LocationWithHidingPlace location in House.Locations) location.OpponentsHiddenHere = new List<Opponent>();
+            SaveGame loadedGame = new SaveGame();
+            string loadedGameResponse = loadedGame.Load(this, nameOfFileToLoad);
+            if (loadedGameResponse != "") return loadedGameResponse;
 
             CurrentLocation = House.GetLocationByName(loadedGame.CurrentLocationName);
             MoveNumber = loadedGame.MoveNumber;
             status = loadedGame.Status;
-            foundOpponents.Clear();
+            FoundOpponents.Clear();
+            List<Opponent> foundOpponents = new List<Opponent>();
             foreach (string opponent in loadedGame.FoundOpponents) foundOpponents.Add(new Opponent(opponent));
+            FoundOpponents = foundOpponents;
 
+            foreach (KeyValuePair<string, string> pair in loadedGame.OpponentsInHidingLocations)
+                (House.GetLocationByName(pair.Value) as LocationWithHidingPlace).OpponentsHiddenHere.Clear();
 
-            Console.WriteLine($"Number of loaded hidden opponnets: {loadedGame.OpponentsInHidingLocations.Count()}");
             foreach (KeyValuePair<string, string> pair in loadedGame.OpponentsInHidingLocations)
                 (House.GetLocationByName(pair.Value) as LocationWithHidingPlace).OpponentsHiddenHere.Add(new Opponent(pair.Key));
 
@@ -155,7 +100,7 @@ namespace Ch10HideAndSeekEndOfChapterProj
                     IEnumerable<Opponent> foundOpponents = location.CheckHidingPlace();
                     if(foundOpponents.Count() > 0)
                     {
-                        this.foundOpponents.AddRange(foundOpponents);
+                        FoundOpponents.AddRange(foundOpponents);
                         return $"You found {foundOpponents.Count()} opponent{House.S(foundOpponents.Count())} hiding {location.HidingPlace}";
                     }
                     else
--- Ch10HideAndSeekEndOfChapterProj/Ch10HideAndSeekEndOfChapterProj/SaveGame.cs
+++ Ch10HideAndSeekEndOfChapterProj/Ch10HideAndSeekEndOfChapterProj/SaveGame.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Newtonsoft.Json;
 
 namespace Ch10HideAndSeekEndOfChapterProj
 {
@@ -10,5 +11,71 @@ namespace Ch10HideAndSeekEndOfChapterProj
         public string CurrentLocationName { get; set; }
         public int MoveNumber { get; set; }
         public string Status { get; set; }
+
+        public string Save(GameController gameController, string nameForSavedFile)
+        {
+            Dictionary<string, string> opponentsInHidingLocations = new Dictionary<string, string>();
+            foreach (Location location in House.Locations)
+                if ((location as LocationWithHidingPlace).HidingPlace != "")
+                    foreach (Opponent opponent in (location as LocationWithHidingPlace).OpponentsHiddenHere)
+                        opponentsInHidingLocations.Add(opponent.Name, location.Name);
+            OpponentsInHidingLocations = opponentsInHidingLocations;
+            CurrentLocationName = gameController.CurrentLocation.Name;
+            MoveNumber = gameController.MoveNumber;
+            Status = gameController.Status;
+            List<string> foundOpponents = new List<string>();
+            foreach (Opponent opponent in gameController.FoundOpponents) foundOpponents.Add(opponent.Name);
+            FoundOpponents = foundOpponents;
+
+            string savedGame = JsonConvert.SerializeObject(this);
+
+            if (!nameForSavedFile.Contains('\\') && !nameForSavedFile.Contains(' '))
+            {
+                var folder = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+                using (StreamWriter writer = new StreamWriter($"{folder}{Path.DirectorySeparatorChar}{nameForSavedFile}.json", false))
+                    writer.WriteLine(savedGame);
+                return $"Saved current game to {nameForSavedFile}.json";
+            }
+            return $"Could not save game to {nameForSavedFile}.json";
+        }
+        public string Load(GameController gameController, string nameOfFileToLoad)
+        {
+            nameOfFileToLoad = nameOfFileToLoad.Split('.')[0];  // if loaded with extension attached to filename, take only the filename
+            if (nameOfFileToLoad.Contains('\\') || nameOfFileToLoad.Contains(' '))
+                return "Could not load game: Invalid characters detected. Please remove backslashes and/or spaces from file name.";
+
+            string gameDataToDeserialize = "";
+            try
+            {
+                // an alternative to the line below it:
+                // string filePath = $"{Environment.GetFolderPath(Environment.SpecialFolder.Personal)}{Path.DirectorySeparatorChar}{nameOfFileToLoad}.json"))
+                string filePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Personal), $"{nameOfFileToLoad}.json");
+                using (StreamReader reader = new StreamReader(filePath))
+                {
+                    gameDataToDeserialize = reader.ReadToEnd();
+                }
+                File.Delete(filePath);
+            }
+            catch
+            {
+                return $"Could not load game: Saved file of name \"{nameOfFileToLoad}.json\" not found";
+            }
+
+            SaveGame? loadedGame = new SaveGame();
+            try
+            {
+                loadedGame = System.Text.Json.JsonSerializer.Deserialize<SaveGame>(gameDataToDeserialize);
+                OpponentsInHidingLocations = loadedGame.OpponentsInHidingLocations;
+                FoundOpponents = loadedGame.FoundOpponents;
+                CurrentLocationName = loadedGame.CurrentLocationName;
+                MoveNumber = loadedGame.MoveNumber;
+                Status = loadedGame.Status;
+            }
+            catch
+            {
+                return $"Could not load game: Could not deserialize file with name \"{nameOfFileToLoad}.json\"";
+            }
+            return "";
+        }
     }
 }
