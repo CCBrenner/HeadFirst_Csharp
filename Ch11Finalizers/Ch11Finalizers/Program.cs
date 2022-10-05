5d91b2e6b0fed11e79541720305dfd860e2c6c7a
Collyn Christopher Brenner <collynbrenner@gmail.com>
2022-10-04 23:55:09 -0500

    added new console app for finalizer

---
+++ Ch11Finalizers/Ch11Finalizers/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Diagnostics;
+class EvilClone
+{
+    public static int CloneCount = 0;
+    public int CloneID { get; } = ++CloneCount;
+    public EvilClone() => Console.WriteLine("Clone #{0} is wreaking havoc.", CloneID);
+    ~EvilClone()
+    {
+        Console.WriteLine("Clone #{0} destroyed", CloneID);
+    }
+}
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Stopwatch stopwatch = Stopwatch.StartNew();
+        List<EvilClone> clones = new List<EvilClone>();
+        while (true)
+        {
+            switch (Console.ReadKey().KeyChar)
+            {
+                case 'a':
+                    clones.Add(new EvilClone());
+                    break;
+                case 'c':
+                    Console.WriteLine("Clearing list at time {0}", stopwatch.ElapsedMilliseconds);
+                    break;
+                case 'g':
+                    Console.WriteLine("Collecting at time {0}", stopwatch.ElapsedMiliseconds);
+            }
+        }
+    }
+}
