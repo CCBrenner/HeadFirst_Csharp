94786c92828edb49c9ed68a6c462ef13363de31f
Collyn Christopher Brenner <collynbrenner@gmail.com>
2023-02-21 12:56:53 -0600

    renamed with Ch numbers, added descriptions for project types, replaced a broken solution

---
+++ Ch10HideAndSeek/Ch10HideAndSeek/ProgramClasses/Direction.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Ch10HideAndSeek
+{
+    public enum Direction
+    {
+        North = -1,
+        South = 1,
+        East = -2,
+        West = 2,
+        Northeast = -3,
+        Southwest = 3,
+        Northwest = -4,
+        Southeast = 4,
+        Up = -5,
+        Down = 5,
+        In = -6,
+        Out = 6,
+    }
+}
